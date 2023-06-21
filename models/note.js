f4312755680a3aad41d64698e74f446a1dbe1e50
lmikszta1 <lmikszta@gmail.com>
2023-06-21 13:50:59 -0400

    1st commit

---
+++ models/note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Note', noteSchema)
